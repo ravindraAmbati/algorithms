6b87f412abfbd09d52261fe7d3b84c324af8f0f7
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-14 19:53:52 +0530

    refactor the code #6

--- src/main/java/com/learn/algorithms/utility/ExtractURLs.java
+++ src/main/java/com/learn/algorithms/utility/ExtractURLs.java
@@ -4,31 +4,31 @@ package com.learn.algorithms.utility;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public interface ExtractURLs {
 
-    static List<URL> extractURLs(String rawHTML, String regex) throws RuntimeException, MalformedURLException {
+    static Set<URL> extractURLs(String rawHTML, String regex) throws RuntimeException, MalformedURLException {
 
-        List<URL> urls = new ArrayList<>();
-        Pattern patt = Pattern.compile(regex);
-        Matcher matcher = patt.matcher(rawHTML);
+        Set<URL> urls = new HashSet<>();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(rawHTML);
         while (matcher.find()) {
             urls.add(new URL(matcher.group()));
         }
         return urls;
     }
 
-    static List<URL> extractHTTPURLs(String rawHTML) throws RuntimeException, MalformedURLException {
+    static Set<URL> extractHTTPURLs(String rawHTML) throws RuntimeException, MalformedURLException {
 
         String regex = "\\b(http?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
         return extractURLs(rawHTML,regex);
     }
 
-    static List<URL> extractHTTPSURLs(String rawHTML) throws RuntimeException, MalformedURLException {
+    static Set<URL> extractHTTPSURLs(String rawHTML) throws RuntimeException, MalformedURLException {
 
         String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
         return extractURLs(rawHTML,regex);
