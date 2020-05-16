94b9df1d3e1cfbb36e6e11c0a12efc1877f65939
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-05-16 12:21:15 +0530

    impl Dijkstra, closes #14

---
+++ src/main/java/com/learn/algorithms/ShortestPath/Dijkstra.java
@@ -0,0 +1,57 @@
+package com.learn.algorithms.ShortestPath;
+
+/* @author ravin @date 16-05-2020 @time 11:27 */
+
+import com.learn.algorithms.ShortestPath.common.Edge;
+import com.learn.algorithms.ShortestPath.common.Node;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Dijkstra implements ShortestPath {
+
+    public List<Node> computeShortestPath(Node source, Node target) {
+        computeShortestPath(source);
+        return getShortestPathTo(target);
+    }
+
+    private void computeShortestPath(Node root) {
+
+        root.setDistance(0d);
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(root);
+
+        while (!priorityQueue.isEmpty()) {
+            Node actual = priorityQueue.poll();
+
+            for (Edge edge : actual.getNeighbours()) {
+
+                Node targetNode = edge.getTarget();
+
+                double newDistance = actual.getDistance() + edge.getWeight();
+
+                if (newDistance < targetNode.getDistance()) {
+
+                    priorityQueue.remove(targetNode);
+                    targetNode.setDistance(newDistance);
+                    targetNode.setPredecessor(actual);
+                    priorityQueue.add(targetNode);
+                }
+            }
+        }
+    }
+
+    private List<Node> getShortestPathTo(Node target) {
+
+        List<Node> shortestPathTo = new ArrayList<>();
+
+        for (Node node = target; null != node; node = node.getPredecessor()) {
+            shortestPathTo.add(node);
+        }
+
+        Collections.reverse(shortestPathTo);
+        return shortestPathTo;
+    }
+}
